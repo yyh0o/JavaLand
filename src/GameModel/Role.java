f660df0ae93c30d383a664baf0297cb33d49725d
yyh <646167686@qq.com>
2018-05-14 13:18:01 +0800

    添加TestTimer类，测试计时器，实现Role move方法

--- src/GameModel/Role.java
+++ src/GameModel/Role.java
@@ -1,8 +1,27 @@
 package GameModel;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
 public class Role extends Biological {
     private Body body;
     private double speed;
-    public void move(){
+    public void move(String toward){
+        if (toward.equals("UP"))
+            this.setY(this.getY() - speed);
+        if (toward.equals("DOWN"))
+            this.setY(this.getY() + speed);
+        if (toward.equals("LEFT"))
+            this.setX(this.getX() - speed);
+        if (toward.equals("RIGHT"))
+            this.setX(this.getX() + speed);
+    }
+    public Role(){
+        speed = 1;
+        setImage(new Image("resource/Role1.png"));
+    }
 
+    public void drawRole(GraphicsContext gc){
+        gc.drawImage(getImage(),getX(),getY());
     }
 }
--- src/GameView/TestTimer.java
+++ src/GameView/TestTimer.java
@@ -1,5 +1,7 @@
 package GameView;
 
+import GameModel.Ground;
+import GameModel.Role;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -18,13 +20,19 @@ public class TestTimer extends Application {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         primaryStage.setTitle("TestTimer");
         primaryStage.setScene(scene);
+
+        Role testRole = new Role();
+        Ground ground = new Ground();
         final long startNanoTime = System.nanoTime();
         new AnimationTimer(){
 
             @Override
             public void handle(long now) {
                 double t = (now - startNanoTime) / 1000000000.0;
-
+                testRole.move("DOWN");
+                testRole.move("RIGHT");
+                ground.drawGroud(gc,32,32);
+                testRole.drawRole(gc);
 
 
             }
