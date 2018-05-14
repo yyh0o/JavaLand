c37d5ae21ddf9241391cc4eb66cbb6ce141376d2
yyh <646167686@qq.com>
2018-05-14 23:10:07 +0800

    加入了事件捕捉，能用键盘控制人物移动。

--- src/GameModel/Role.java
+++ src/GameModel/Role.java
@@ -7,17 +7,25 @@ public class Role extends Biological {
     private Body body;
     private double speed;
     public void move(String toward){
-        if (toward.equals("UP"))
+        if (toward.equals("UP")){
+//            this.setImage(new Image(""));
             this.setY(this.getY() - speed);
-        if (toward.equals("DOWN"))
+        }
+        if (toward.equals("DOWN")){
+            this.setImage(new Image("resource/Role1.png"));
             this.setY(this.getY() + speed);
-        if (toward.equals("LEFT"))
+        }
+        if (toward.equals("LEFT")){
+            this.setImage(new Image("resource/Role1Left.png"));
             this.setX(this.getX() - speed);
-        if (toward.equals("RIGHT"))
+        }
+        if (toward.equals("RIGHT")){
+            this.setImage(new Image("resource/Role1Right.png"));
             this.setX(this.getX() + speed);
+        }
     }
     public Role(){
-        speed = 1;
+        speed = 3;
         setImage(new Image("resource/Role1.png"));
     }
 
--- src/GameView/TestTimer.java
+++ src/GameView/TestTimer.java
@@ -4,11 +4,15 @@ import GameModel.Ground;
 import GameModel.Role;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class TestTimer extends Application {
     @Override
@@ -23,14 +27,47 @@ public class TestTimer extends Application {
 
         Role testRole = new Role();
         Ground ground = new Ground();
+
+        ArrayList<String> input = new ArrayList<String>();
+
+        scene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent event) {
+                        String code = event.getCode().toString();
+
+                        if (!input.contains(code))
+                            input.add(code);
+                    }
+                }
+        );
+
+        scene.setOnKeyReleased(
+                new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent event) {
+                        String code = event.getCode().toString();
+                        input.remove(code);
+                    }
+                }
+        );
+
         final long startNanoTime = System.nanoTime();
         new AnimationTimer(){
 
             @Override
             public void handle(long now) {
                 double t = (now - startNanoTime) / 1000000000.0;
-                testRole.move("DOWN");
-                testRole.move("RIGHT");
+//                testRole.move("DOWN");
+//                testRole.move("RIGHT");
+                if (input.contains("UP") || input.contains("W"))
+                    testRole.move("UP");
+                if (input.contains("DOWN") || input.contains("S"))
+                    testRole.move("DOWN");
+                if (input.contains("LEFT") || input.contains("A"))
+                    testRole.move("LEFT");
+                if (input.contains("RIGHT") || input.contains("D"))
+                    testRole.move("RIGHT");
                 ground.drawGroud(gc,32,32);
                 testRole.drawRole(gc);
 
